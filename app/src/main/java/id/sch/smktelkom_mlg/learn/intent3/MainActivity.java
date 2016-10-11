696a6119ccd9be9d03ab26dd151d8a286052d6ea
Resta <resta190498@gmail.com>
2016-10-11 20:49:56 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -13,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("http://www.smktelkom-mlg.sch.id/");
+            }
+        });
+
         findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
                 dialPhoneNumber("0341712500");
             }
         });
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 
     public void composeSmsMessage(String message) {
